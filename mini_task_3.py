a799bec839fb770e993e320a27ece78b71888fd2
kshiroky <nshiroky@yandex.ru>
2022-07-26 23:58:57 +0300

    resolved conflict

--- mini_task_3.py
+++ mini_task_3.py
@@ -41,8 +41,8 @@ Ccat - концентрация наночастиц (mkg/ml), при котор
 
 
 
-pip install category-encoders
-pip install shap
+# pip install category-encoders
+# pip install shap
 
 
 import pandas as pd
@@ -55,17 +55,42 @@ from sklearn.ensemble import RandomForestRegressor as forestreg
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import MinMaxScaler 
-from catboost import CatBoostClassifier, Pool, cv
+from catboost import CatBoostRegressor, Pool, cv
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 from category_encoders import TargetEncoder
-import shap
+#mport shap
 
 from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import train_test_split as splt
 
 from sklearn.model_selection import cross_val_predict
+
+
+
+key = 'T8DoTl6EKcwUpXqg'
+from pymatgen.ext.matproj import MPRester
+m = MPRester(key)
+syng_trans = {0: 'amorphous',  1: 'triclinic', 2: 'monoclinic', 3: 'orthorhombic',
+              4: 'tetragonal', 5: 'trigonal',  6: 'hexagonal',  7: 'cubic'}
+def get_attribute(formula, syng, atri='energy'):
+    try:
+        formula = m.query(formula.replace('α-','').replace('β-','').replace('a-','').replace('b',''), ['pretty_formula'])
+        attribute = m.query({'$and': [{'pretty_formula': formula[0]['pretty_formula']}, {'crystal_system': syng_trans[syng]}]}, [atri])
+        summ = 0
+        for i in range(len(attribute)):
+            summ += attribute[i][atri]
+        return summ 
+    except:
+        return np.nan
+def add_prop(df, prop='energy'):
+    df = df.copy()
+    new_col = []
+    for i in range(len(df.formula)):
+        new_col.append(get_attribute(df['formula'][i], df['Syngony'][i]))
+    df[prop] = new_col
+    return df
 
 
 url = 'https://raw.githubusercontent.com/kshiroky/DataCon/main/task%203.csv'
@@ -81,7 +106,6 @@ raw_data['Kcat'] = raw_data.Kcat.str.replace(' ','').astype(float)
 # корреляционная матрица
 cols_to_analyse= list(set(column_ls) - set(['formula', 'Subtype']))
 data_corr=raw_data[cols_to_analyse].corr()
-
 plt.figure(figsize = (10,6))
 sns.heatmap(data_corr)
 plt.show()
@@ -114,8 +138,9 @@ plt.show()
 
 
 # нормализация MinMax
-x = exemplary_df.values
-cols = exemplary_df.columns
+to_scale_df = exemplary_df.drop(['Km', 'Kcat'], axis = 1)
+x = to_scale_df.values
+cols = to_scale_df.columns
 min_max_scaler = MinMaxScaler()
 x_scaled = min_max_scaler.fit_transform(x)
 norm_df = pd.DataFrame(x_scaled, columns=cols)
@@ -131,11 +156,12 @@ print(norm_df)
 
 
 #split data before model selection
-km_X_train, km_X_test, km_y_train, km_y_test = splt(norm_df.drop(['Km','Kcat'], axis = 1), norm_df['Km'], shuffle = False, random_state = 50, test_size = 0.3)
-kcat_X_train, kcat_X_test, kcat_y_train, kcat_y_test = splt(norm_df.drop(['Km','Kcat'], axis = 1), norm_df['Kcat'], shuffle = False, random_state = 50, test_size = 0.3)
-x_data = norm_df.drop(['Km','Kcat'], axis = 1)
-km_y_data = norm_df['Km']
-kcat_y_data = norm_df['Kcat']
+from sklearn.model_selection import train_test_split as splt
+km_X_train, km_X_test, km_y_train, km_y_test = splt(norm_df, exemplary_df['Km'], shuffle = False, random_state = 50, test_size = 0.3)
+kcat_X_train, kcat_X_test, kcat_y_train, kcat_y_test = splt(norm_df, exemplary_df['Kcat'], shuffle = False, random_state = 50, test_size = 0.3)
+x_data = norm_df
+km_y_data = exemplary_df['Km']
+kcat_y_data = exemplary_df['Kcat']
 
 #evaluate the shape of splitted data
 print(km_X_train.shape, km_y_train.shape)
@@ -148,13 +174,24 @@ print(x_data.shape, kcat_y_data.shape)
 #Km model parameters
 km_forest = forestreg(n_estimators = 100, random_state = 50, oob_score = True)
 km_forest.fit(km_X_train, km_y_train)
+#feature importance
+features = km_forest.feature_names_in_
+importances = km_forest.feature_importances_
+indices = np.argsort(importances)
+
+plt.title('Feature Importances')
+plt.barh(range(len(indices)), importances[indices], color='#8f63f4', align='center')
+plt.yticks(range(len(indices)), features[indices])
+plt.xlabel('Relative Importance')
+plt.show()
+
 #visualize the results of training
 km_plot = plt.figure(figsize = (10,6))
 sns.scatterplot(np.log10(km_y_train), np.log10(km_forest.oob_prediction_))
 plt.xlabel('real')
 plt.ylabel('predict')
 plt.title('prediciton without cross-validation for Km')
-x_lin = np.linspace(-7, 2, 100)
+x_lin = np.linspace(-4, 4, 100)
 y_lin = x_lin
 plt.plot(x_lin, y_lin, 'r')
 plt.show()
@@ -180,10 +217,23 @@ plt.show()
 #Kcat model parameters
 kcat_forest = forestreg(n_estimators = 100, random_state = 50, oob_score = True)
 kcat_forest.fit(kcat_X_train, kcat_y_train)
+#feature importance
+features = km_forest.feature_names_in_
+importances = km_forest.feature_importances_
+indices = np.argsort(importances)
+
+plt.title('Feature Importances')
+plt.barh(range(len(indices)), importances[indices], color='#8f63f4', align='center')
+plt.yticks(range(len(indices)), features[indices])
+plt.xlabel('Relative Importance')
+plt.show()
 
 #visualize the results of training
 kcat_for_plt = plt.figure(figsize = (10,6))
 sns.scatterplot(np.log10(kcat_y_train), np.log10(kcat_forest.oob_prediction_))
+x_lin = np.linspace(-7, 8, 100)
+y_lin = x_lin
+plt.plot(x_lin, y_lin, 'r')
 plt.title('no cross-val random forest for Kcat')
 plt.show()
 
@@ -214,12 +264,6 @@ cbr.fit(km_X_train, km_y_train, plot=True)
 km_train = km_X_train.merge(km_y_train, right_index = True, left_index = True)
 categorical_features_indices = np.where(km_train.dtypes != np.float)[0]
 
-#feature importance according to cat
-shap.initjs()
-explainer = shap.TreeExplainer(cbr)
-shap_values = explainer.shap_values(Pool(km_X_train, km_y_train, cat_features=categorical_features_indices))
-shap.force_plot(explainer.expected_value, shap_values[0,:], km_X_train.iloc[0,:], matplotlib=True, show=True)
-shap.summary_plot(shap_values, km_X_train, plot_type="bar")
 
 # prediction according to cat
 Km_y_pred_cbr = cbr.predict(km_X_test)
@@ -231,6 +275,24 @@ sns.regplot(km_y_test, Km_y_pred_cbr, fit_reg=True, scatter_kws={"s": 100})
 print(cbr.get_best_score())
 print(cbr.get_scale_and_bias())
 
+#cv for catboost
+from catboost import Pool, cv
+params = {"iterations": 100,
+          "depth": 2,
+          "loss_function": "RMSE",
+          "verbose": False}
+
+km_cv_dataset = Pool(data= x_data, #change to a propriate one
+                  label= km_y_data) #also change
+km_scores = cv(km_cv_dataset,
+            params,
+            fold_count=5)
+
+km_cat_scores_plot = plt.figure(figsize= (10, 6))
+sns.displot(km_scores)
+plt.title('score of cat cross-val for Km')
+plt.show()
+
 
 # MODEL FOR KCAT WITH CATBOOST
 # kcat_X_train, kcat_X_test, kcat_y_train, kcat_y_test = splt(norm_df.drop(['Km','Kcat'], axis = 1), norm_df['Kcat'], shuffle = False, random_state = 50, test_size = 0.3)
@@ -239,12 +301,6 @@ cbr.fit(kcat_X_train, kcat_y_train, plot=True)
 kcat_train = kcat_X_train.merge(kcat_y_train, right_index = True, left_index = True)
 categorical_features_indices = np.where(kcat_train.dtypes != np.float)[0]
 
-#feature importance according to cat
-shap.initjs()
-explainer = shap.TreeExplainer(cbr)
-shap_values = explainer.shap_values(Pool(kcat_X_train, kcat_y_train, cat_features=categorical_features_indices))
-shap.force_plot(explainer.expected_value, shap_values[0,:], kcat_X_train.iloc[0,:], matplotlib=True, show=True)
-shap.summary_plot(shap_values, kcat_X_train, plot_type="bar")
 
 # prediction according to cat
 Kcat_y_pred_cbr = cbr.predict(km_X_test)
@@ -255,3 +311,22 @@ sns.regplot(kcat_y_test, Kcat_y_pred_cbr, fit_reg=True, scatter_kws={"s": 100})
 # getting RMSE and bias
 print(cbr.get_best_score())
 print(cbr.get_scale_and_bias())
+
+#cv for catboost
+from catboost import Pool, cv
+params = {"iterations": 100,
+          "depth": 2,
+          "loss_function": "RMSE",
+          "verbose": False}
+
+kcat_cv_dataset = Pool(data= x_data, #change to a propriate one
+                  label= kcat_y_data) #also change
+kcat_scores = cv(kcat_cv_dataset,
+            params,
+            fold_count=5)
+
+kcat_cat_scores_plot = plt.figure(figsize= (10, 6))
+sns.displot(kcat_scores)
+plt.title('score of cat cross-val for Kcat')
+plt.show()
+
